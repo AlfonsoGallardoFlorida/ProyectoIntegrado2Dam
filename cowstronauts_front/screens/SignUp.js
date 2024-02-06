7cd601194ddd424f4dc30e2fa1aa6e6b827c0761
Oscar Lopez <oslolu01@floridauniversitaria.es>
2024-02-06 15:38:21 +0100

    creacion de interfaz de login y signup

---
+++ cowstronauts_front/screens/SignUp.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet, Image, KeyboardAvoidingView, Platform } from 'react-native';
+
+const SignUp = () => {
+  return (
+   
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={require('../assets/img/logo.png')} style={styles.logo} />
+      </View>
+
+      <View style={styles.loginForm}>
+        <TextInput
+          style={styles.inputField}
+          placeholder="Username"
+          placeholderTextColor="#ccc"
+        />
+        <TextInput
+          style={styles.inputField}
+          placeholder="Email"
+          placeholderTextColor="#ccc"
+          secureTextEntry
+        />
+        <TextInput
+          style={styles.inputField}
+          placeholder="Password"
+          placeholderTextColor="#ccc"
+        />
+        <TextInput
+          style={styles.inputField}
+          placeholder="Repeat Password"
+          placeholderTextColor="#ccc"
+          secureTextEntry
+        />
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={{ color: '#FFFFFF', fontSize: 18 }}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <Image source={require('../assets/img/martesignup.png')} style={styles.marteImage} />
+
+      <View style={styles.footerTextContainer}>
+        <Text style={styles.footerText}>Have an account?</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C1463F',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  loginForm: {
+    flex: 1,
+    width: '65%',
+    textAlign: 'center',
+    marginBottom: 275,
+  },
+  inputField: {
+    width: '100%',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: '#fff',
+  },
+  loginButton: {
+    width: '50%',
+    padding: 10,
+    backgroundColor: '#FEAA16',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  
+  marteImage: {
+    position: 'absolute',
+    top: 610,
+    bottom: 0,
+    right: 0,
+    width: 125,  
+    height: 125, 
+    resizeMode: 'contain',
+  },
+  footerTextContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 25,
+    margin: 5,
+  },
+  footerText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  },
+});
+
+export default SignUp;
