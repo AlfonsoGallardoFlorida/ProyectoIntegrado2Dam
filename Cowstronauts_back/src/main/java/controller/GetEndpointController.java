1edca364116604ed0cc848b2b81f04a81fa72299
Tino <97448098+Tiinocm@users.noreply.github.com>
2024-02-09 18:10:55 +0100

    login devuleve el user

--- Cowstronauts_back/src/main/java/controller/GetEndpointController.java
+++ Cowstronauts_back/src/main/java/controller/GetEndpointController.java
@@ -34,9 +34,9 @@ public class GetEndpointController {
 		System.out.println(listUsers);
 		for (Users u : listUsers) {
 			String name = u.getName().toLowerCase();
-			System.out.println(name);
 			if (name.equals(user.toLowerCase())) {
 				if (checkPassword(u.getPassword(), pass)) {
+					jsonString.put("data", u);
 					return ResponseEntity.status(HttpStatus.OK).body(jsonString);
 				} else {
 					return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(jsonString);
