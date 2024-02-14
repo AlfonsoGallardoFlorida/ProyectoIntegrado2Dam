a60a3721a90aaaccc620a1a583175445c45a6cec
Oscar Lopez <oslolu01@floridauniversitaria.es>
2024-02-14 17:44:43 +0100

    actualizando tienda de nave

--- cowstronauts_expo/src/screens/shops/Shop2.js
+++ cowstronauts_expo/src/screens/shops/Shop2.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -10,55 +10,87 @@ import {
   Dimensions,
 } from 'react-native';
 import { Audio } from 'expo-av';
+import Icon from 'react-native-vector-icons/FontAwesome'; 
+//import svgEarth from '../../../assets/img/svg/svgEarth';
 
 const App = () => {
+  const [upgradePurchased, setUpgradePurchased] = useState(false);
   const play = async () => {
     const { sound } = await Audio.Sound.createAsync(
       require('../../../assets/sound/Josh.exe.mp3')
     );
     await sound.playAsync();
   };
-
+  // <svgEarth />
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.title}>SHOP</Text>
+        <Text style={styles.titleShip}>SHIP</Text>
+        <Text style={styles.titleUp}>UPGRADES</Text>
       </View>
 
       <ImageBackground
-        source={require('../../../assets/img/planets/jupiter.png')}
+        source={require('../../../assets/img/planets/earth.png')}
         resizeMode="cover"
         style={styles.backgroundImage}>
-        <ScrollView contentContainerStyle={styles.scrollViewContent}>
-          <TouchableOpacity onPress={play} style={styles.product}>
-            <View style={styles.productImageContainer}>
-              <Image
-                source={require('../../../assets/img/upgrades_cps/images/Josh.jpg')}
-                style={styles.productImage}
-              />
-            </View>
-            <View style={styles.productInfo}>
-              <Text style={styles.productTitle}>JOSH HUTCHERSON</Text>
+        <View style={styles.scrollViewContent}>
+          <ScrollView style={{ height: windowHeight - 200 }}>
+            <TouchableOpacity onPress={() => setUpgradePurchased(true)} style={styles.product}>
+              <View style={styles.productImageContainer}>
+                <Image
+                  source={require('../../../assets/img/upgrades_cps/images/Josh.jpg')}
+                  style={styles.productImage}
+                />
+              </View>
+              <View style={styles.productInfo}>
+                <Text style={styles.productTitle}>JOSH HUTCHERSON</Text>
+                <Text style={styles.productDescription}>
+                  JoshHutchersonJoshHutchersonJoshHutchersonJoshHutchersonJoshHutcherson
+                </Text>
+                <Text style={styles.coinsUpgrade}>
+                  {upgradePurchased ? (
+                    <Icon name="check" size={20} color="green" />
+                  ) : (
+                    <>
+                      1000 &nbsp;
+                      <Image
+                        source={require('../../../assets/img/logos/zloty.png')}
+                        style={styles.coinImage}
+                      />
+                    </>
+                  )}
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={play} style={styles.product}>
+              <View style={styles.productImageContainer}>
+                <Image
+                  source={require('../../../assets/img/upgrades_cps/images/Josh.jpg')}
+                  style={styles.productImage}
+                />
+              </View>
+              <View style={styles.productInfo}>
+                <Text style={styles.productTitle}>SOY MILK</Text>
+                <Text style={styles.productDescription}>
+                  Les vaques produiran el doble durant 3 minuts
+                </Text>
+                <Text style={styles.coinsUpgrade}>
+                  1000 &nbsp;
+                  <Image
+                    source={require('../../../assets/img/logos/zloty.png')}
+                    style={styles.coinImage}
+                  />
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <View style={styles.product}>
+              <Text style={styles.productTitle}>VACA TANICA</Text>
               <Text style={styles.productDescription}>
-                JoshHutchersonJoshHutchersonJoshHutchersonJoshHutchersonJoshHutchersonJoshHutcherson
+                Probabilitat del 32% que aparega una vaca satànica
               </Text>
             </View>
-          </TouchableOpacity>
-
-          <View style={styles.product}>
-            <Text style={styles.productTitle}>SOY MILK</Text>
-            <Text style={styles.productDescription}>
-              Les vaques produiran el doble durant 3 minuts
-            </Text>
-          </View>
-
-          <View style={styles.product}>
-            <Text style={styles.productTitle}>VACA TANICA</Text>
-            <Text style={styles.productDescription}>
-              Probabilitat del 32% que aparega una vaca satànica
-            </Text>
-          </View>
-        </ScrollView>
+          </ScrollView>
+        </View>
       </ImageBackground>
       <View style={styles.navigation} />
     </View>
@@ -77,26 +109,35 @@ const styles = StyleSheet.create({
     height: 115,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: 5,
   },
-  title: {
+  titleShip: {
     color: '#8C81A7',
-    fontSize: 50,
+    fontSize: 40,
     fontWeight: 'bold',
     textAlign: 'center',
-    paddingTop: 60,
+    paddingTop: 65,
+  },
+  titleUp: {
+    color: '#8C81A7',
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   backgroundImage: {
     width: windowWidth,
-    height: windowHeight * 0.85, 
+    height: windowHeight * 0.85,
     justifyContent: 'center',
   },
   scrollViewContent: {
     flexGrow: 1,
     alignItems: 'center',
-    paddingTop: 140, 
+    paddingTop: 5,
+    maxHeight: 400,
+    width: '100%',
   },
   product: {
-    width: '90%',
+    width: '85%',
     padding: 10,
     marginVertical: 5,
     backgroundColor: '#2B2930',
@@ -104,9 +145,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     elevation: 4,
     overflow: 'hidden',
+    marginLeft: 7,
   },
   productImageContainer: {
-    width: 100, 
+    width: 100,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -116,7 +158,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   productInfo: {
-    flex: 1, 
+    flex: 1,
     marginLeft: 10,
   },
   productTitle: {
@@ -133,6 +175,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 20,
+  },
+  coinImage: {
+    width: 15,
+    height: 15,
+  },
+  coinsUpgrade: {
+    color: 'white',
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
   },
 });
 
